aba9c7b3f8708d66aa49d6e0a29124ababd971ff
IAP00011001 <673282096@qq.com>
2021-10-25 21:38:56 +0800

    2021.10.25提交

---
+++ _public.c
@@ -0,0 +1,14 @@
+#include  "_public.h"
+
+int maxx(int num1, int num2)
+{
+	if (num1>num2) return num1;
+	else return num2;
+}
+
+
+int minn(int num1, int num2)
+{
+	if (num1>num2) return num2;
+	else return num1;
+}
---
+++ _public.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 声明max函数，取最大值
+int maxx(int num1, int num2);
+
+// 声明min函数，取最小值
+int minn(int num1, int num2);
